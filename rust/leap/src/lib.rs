a3f5d3ca3d90dfbb043626ca9a11e15385356c7c
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-08 19:49:19 -0500

    Submit Leap

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    // unimplemented!("true if {} is a leap year", year)
+    if year % 400 == 0 {
+        true
+    } else if year % 100 == 0 {
+        false
+    } else if year % 4 == 0 {
+        true
+    } else {
+        false
+    }
 }

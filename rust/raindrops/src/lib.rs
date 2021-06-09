4c3ddf90aaed2d3b2ca645f597bc236131b23ee7
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-08 21:35:08 -0500

    Submit Raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    // unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut out = String::new();
+
+    if n % 3 == 0 {
+        out += "Pling";
+    }
+    if n % 5 == 0 {
+        out += "Plang";
+    }
+    if n % 7 == 0 {
+        out += "Plong";
+    }
+    if out.is_empty() {
+        out = n.to_string();
+    }
+    out
 }

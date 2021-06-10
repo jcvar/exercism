e9b309b4afa4e16d04d4efde5b5e789f40207d83
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-10 01:58:13 -0500

    Submit Grains

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,15 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    // unimplemented!("grains of rice on square {}", s);
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    }
+    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    // unimplemented!();
+    (1..=64).fold(0, |mut s, i| -> u64 {
+        s += square(i);
+        s
+    })
 }

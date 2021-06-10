87e5f2a198099f4b5afac6505d68e34d42cb13fa
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-10 01:26:06 -0500

    Submit Sum of Factors

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,7 +1,20 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    // unimplemented!(
+    //     "Sum the multiples of all of {:?} which are less than {}",
+    //     factors,
+    //     limit
+    // )
+    let mut sum: u32 = 0;
+    for i in 1..limit {
+        for j in factors {
+            if *j == 0 {
+                continue;
+            }
+            if i % j == 0 {
+                sum += i;
+                break;
+            }
+        }
+    }
+    sum
 }

6afb8304de26a06eb478d4d66e9b297d91f7dc92
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-09 23:29:34 -0500

    Submit Proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    // unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut out = String::new();
+    if list.len() > 0 {
+        for i in 1..list.len() {
+            out += &format!("For want of a {} the {} was lost.\n", list[i - 1], list[i]);
+        }
+        out += &format!("And all for the want of a {}.", list[0]);
+    }
+    out
 }

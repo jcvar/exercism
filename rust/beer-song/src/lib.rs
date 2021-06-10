1537d9f76574bfa78e9386faa5957f894d3f66f7
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-09 23:11:00 -0500

    Submit Beer Song

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,38 @@
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    // unimplemented!("emit verse {}", n)
+    let mut v = String::new();
+    if n == 0 {
+        v += "No more bottles of beer on the wall, no more bottles of beer.\n";
+        v += "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+    } else {
+        let b = bottles(n);
+        v += &format!("{} of beer on the wall, {} of beer.\n", b, b);
+        v += &format!(
+            "Take {} down and pass it around, {} of beer on the wall.\n",
+            if n == 1 { "it" } else { "one" },
+            bottles(n - 1)
+        );
+    }
+    v
+}
+
+pub fn bottles(n: u32) -> String {
+    match n {
+        0 => format!("no more bottles"),
+        1 => format!("1 bottle"),
+        _ => format!("{} bottles", n),
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    // unimplemented!("sing verses {} to {}, inclusive", start, end)
+    let mut bottle = start;
+    let mut out = String::new();
+    while bottle > end {
+        out += &verse(bottle);
+        out += "\n";
+        bottle -= 1;
+    }
+    out += &verse(bottle);
+    out
 }

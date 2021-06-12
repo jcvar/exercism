3853ff9a6d45795574323db4ed77661d14aa5dae
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-12 10:55:42 -0500

    Submit Armstrong Numbers

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    // unimplemented!("true if {} is an armstrong number", num)
+    let mut n = num;
+    let mut p = 0;
+    let mut arm = Vec::new();
+    while n > 0 {
+        arm.push(n%10);
+        n/=10;
+        p+=1;
+    }
+    num == arm.iter().map(|a| a.pow(p)).sum()
 }

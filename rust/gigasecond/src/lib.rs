be85bfac35d47208b2db8d30caacd38a84527cad
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-17 00:27:28 -0500

    Submit Gigasecond

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,7 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    // unimplemented!("What time is a gigasecond later than {}", start);
+    Utc.timestamp(start.timestamp() + 1_000_000_000, 0)
 }

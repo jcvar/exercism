f1c249eb76f09d11a8971c1ba6ba6e5650d54951
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-16 22:40:01 -0500

    Submit Reverse String

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,12 @@
+extern crate unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    // unimplemented!("Write a function to reverse {}", input);
+    let mut output = String::with_capacity(input.len());
+    let mut graphemes = input.graphemes(true);
+    while let Some(g) = graphemes.next_back() {
+        output += g;
+    }
+    output
 }

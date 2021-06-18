e88f5c4ee1f20f0c7c9a31c11cbcf74c502d9fee
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-18 01:07:43 -0500

    Submit Collatz Conjecture

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,6 +1,22 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    // unimplemented!(
+    //     "return Some(x) where x is the number of steps required to reach 1 starting with {}",
+    //     n,
+    // )
+    if n == 0 {
+        return None;
+    }
+
+    let mut i = 0;
+    let mut x = n;
+
+    while x > 1 {
+        if x % 2 == 0 {
+            x /= 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        i += 1;
+    }
+    Some(i)
 }

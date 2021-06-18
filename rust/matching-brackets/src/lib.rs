3908006f45dc541cf4de70a1ae9b9a68dfd95c9b
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-18 00:02:01 -0500

    Submit Matching Brackets

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,18 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    // unimplemented!(
+    //     "Check if the string \"{}\" contains balanced brackets",
+    //     string
+    // );
+    let mut stack = Vec::new();
+    for s in string.chars() {
+        match (stack.last(), s) {
+            (_, '(') | (_, '[') | (_, '{') => stack.push(s),
+            (Some('('), ')') | (Some('['), ']') | (Some('{'), '}') => {
+                stack.pop();
+            }
+            (_, ')') | (_, ']') | (_, '}') => return false,
+            _ => continue,
+        }
+    }
+    stack.is_empty()
 }

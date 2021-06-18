0f1ae504c76afa2514c03089df11eec02eb54d9c
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-18 00:38:55 -0500

    Submit Series

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    unimplemented!(
-        "What are the series of length {} in string {:?}",
-        len,
-        digits
-    )
+    // unimplemented!(
+    //     "What are the series of length {} in string {:?}",
+    //     len,
+    //     digits
+    // )
+    let mut series = Vec::new();
+    if len <= digits.len() {
+        for i in 0..=(digits.len() - len) {
+            if let Some(s) = digits.get(i..i + len) {
+                series.push(s.to_string());
+            }
+        }
+    }
+    series
 }

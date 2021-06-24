4a0e490e81687cd40ae860c6a679f4379f1796ab
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-24 00:00:26 -0500

    Submit Diffie-Hellman

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,21 +1,44 @@
+use rand::Rng;
+
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    // unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    rand::thread_rng().gen_range(2..p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate public key using prime numbers {} and {}, and private key {}",
-        p,
-        g,
-        a
-    )
+    // unimplemented!(
+    //     "Calculate public key using prime numbers {} and {}, and private key {}",
+    //     p,
+    //     g,
+    //     a
+    // )
+    mod_pow(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate secret key using prime number {}, public key {}, and private key {}",
-        p,
-        b_pub,
-        a
-    )
+    // unimplemented!(
+    //     "Calculate secret key using prime number {}, public key {}, and private key {}",
+    //     p,
+    //     b_pub,
+    //     a
+    // )
+    mod_pow(b_pub, a, p)
+}
+
+pub fn mod_pow(b: u64, e: u64, m: u64) -> u64 {
+    if m == 1 {
+        0
+    } else {
+        let mut exp = e;
+        let mut bse = b % m;
+        let mut res = 1;
+        while exp > 0 {
+            if exp % 2 == 1 {
+                res = (res * bse) % m;
+            }
+            exp /= 2;
+            bse = bse.pow(2) % m;
+        }
+        res
+    }
 }
